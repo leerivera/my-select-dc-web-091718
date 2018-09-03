a3663240cfd2cfc694c1f1fc49fb6686086e0835
leerivera80@gmail.com Rivera <leerivera80@gmail.com>
2018-09-03 18:04:35 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,15 +1,15 @@
 nums = [1, 2, 3, 4, 5] 
 
 def my_select(collection) 
-  empty_arr = [] 
+  new_arr = [] 
   
   i=0 
   while i < collection.length
-  empty_arr.push yield(collection[i]) 
+  new_arr.push yield(collection[i]) 
   i += 1 
 end 
 
-  empty_arr.compact 
+ new_arr
 end 
 
 

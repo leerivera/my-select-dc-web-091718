992a6324ab4bb134bc35e20ae31e3c1514eb21b0
leerivera80@gmail.com Rivera <leerivera80@gmail.com>
2018-09-03 18:01:06 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -16,3 +16,21 @@ my_selectn(num) do |num|
     num
   end
 end
+nums = [1, 2, 3, 4, 5] 
+
+def my_select(array) 
+  i =0 
+  select = [] 
+  while i < array.length 
+    if (yield(array[i]))
+      # select.push(array[i])OR 
+      select << array[i] 
+    end 
+    i += 1 
+  end 
+  select 
+end 
+
+my_select(nums) do |num| 
+  num.even? 
+end 
